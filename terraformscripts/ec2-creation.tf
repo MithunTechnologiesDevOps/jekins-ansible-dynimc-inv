8e8b5b177c6e3a5bbac67b7a696f1b3b5a4c9178
devopstrainingblr <devopstrainingblr@gmail.com>
2022-09-25 20:49:14 +0530

    added terraform script

---
+++ terraformscripts/ec2-creation.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+   region  = "ap-south-1" 
+   
+}
+
+resource "aws_instance" "AWSEC2Instance"{
+     ami     =  "ami-05c8ca4485f8b138a"
+     instance_type = "t2.micro"
+     security_groups = ["launch-wizard-2"]
+     key_name        = "devopsjulybatch2022"
+     tags = {
+        Name = "tomcatservers"
+     }
+}
+
