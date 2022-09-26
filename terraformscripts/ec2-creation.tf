f617e3658c88da8b3aa4a3ecbd39a473ac9182fc
Mithun Technologies <devopstrainingblr@gmail.com>
2022-09-26 07:20:05 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 }
 
 resource "aws_instance" "AWSEC2Instance"{
-     count   = 1
+     count   = 2
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
