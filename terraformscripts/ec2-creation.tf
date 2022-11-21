77aef54926e40ab2b90f964accb12f0fa507c39d
Mithun Technologies <devopstrainingblr@gmail.com>
2022-11-21 21:43:17 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami = "ami-027ceada28bc1f562"
+     ami = "ami-0b2aec26bb1a5169d"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
      key_name        = "devopsmss"
