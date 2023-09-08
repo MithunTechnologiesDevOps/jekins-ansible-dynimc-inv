bec121fb4b1f9d7e93df10d9db649ae2217eb89c
Mithun Technologies <devopstrainingblr@gmail.com>
2023-09-08 06:29:18 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,12 +2,13 @@ provider "aws"{
 region = "ap-south-1"
 }
 
+
 resource "aws_instance" "AWSServer"{
-ami = "ami-0da59f1af71ea4ad2"
+ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-key_name = "devopsmithuntechnologiesbhaskar"
 security_groups = ["launch-wizard-2"]
+key_name = "devopsmithuntechnologies"
 tags = {
-Name = "tomcatservers"
+Name =  "tomcatservers"
 }
 }
