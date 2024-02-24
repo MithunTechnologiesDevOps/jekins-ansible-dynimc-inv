57529b603aeb04a51dff85699432e3fd8700f926
Mithun Technologies <devopstrainingblr@gmail.com>
2024-02-24 06:57:42 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,7 +7,7 @@ resource "aws_instance"  "AWSServer"{
 ami = "ami-0e670eb768a5fc3d4"
 instance_type = "t2.micro"
 key_name = "devopsdecbatchmss"
-security_groups = ['launch-wizard-2']
+security_groups = ["launch-wizard-2"]
 tags = {
 Name = "TerraformServer"
 }
