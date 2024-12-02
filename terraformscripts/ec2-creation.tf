f385c0018c32dc51fba3937fbb025d5114b41c8a
Mithun Technologies <devopstrainingblr@gmail.com>
2024-12-02 07:18:59 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance"  "AWSInstance" {
    ami = "ami-0614680123427b75e"
    instance_type = "t2.micro"
-   key_name = "ami-0614680123427b75e"
+   key_name = "devopsnov2024"
    security_groups = ["launch-wizard-2"]
    tags={
    Name = "terraformserver"
