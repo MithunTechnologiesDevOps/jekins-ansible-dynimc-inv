3f95693ecaa6283d04f36c680b33b8780f0d8c34
Mithun Technologies <devopstrainingblr@gmail.com>
2025-07-24 20:19:47 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,12 +2,13 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance"{
- ami = "ami-0af9569868786b23a"
- instance_type = "t2.medium"
- key_name = "devopsmithuntechnologies2025"
+
+resource "aws_instance"  "AWSInstance" {
+ ami = "ami-0b32d400456908bf9"
+ instance_type = "t2.micro"
+ key_name = "mithuntechnologiesdevopsaws2025"
  security_groups = ["launch-wizard-2"]
  tags = {
- Name = "tomcatservers"
+ Name = "terraformserver"
  }
 }
