41fb01d0b0093d7eac023c7c505c7b65aeac6188
Mithun Technologies <devopstrainingblr@gmail.com>
2026-01-14 06:29:58 +0530

    Update EC2 instance configuration in Terraform script

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,14 @@
-provider "aws"{
-region = "ap-southeast-1"
+provider "aws" {
+    region = "ap-southeast-1"
 }
 
-resource "aws_instance" "AWSEC2INSTANCE"{
-ami = "ami-0c56f26c1d3277bcb"
-instance_type = "t2.micro"
-key_name = "mithuntechnologiesdevops"
-security_groups = ["launch-wizard-2"]
-tags = {
-Name = "terraformserver"
+resource "aws_instance" "AWSEC2Instance" {
+  ami  = "ami-01dc51e87421923b6"
+  instance_type = "t2.micro"
+  key_name = "mithuntechnologiesdevops" 
+  security_groups = ["launch-wizard-2"]
+  tags = {
+    Name = "TerraformServer"
+  } 
 }
-}
+
